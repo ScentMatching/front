ab87347a20a721bf2e0ec8944c10811ac563f97e
ryugwanghyoen <rgh4422@google.com>
2024-10-16 10:24:59 +0900

    feat: 질문,답변 생성 및 결과 mbti도출

--- scent/pages/perfumeTest/index.tsx
+++ scent/pages/perfumeTest/index.tsx
@@ -1,27 +1,112 @@
+import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { questions } from "@/constants/question";
+import { answers } from "@/constants/Answers";
 
-const index = () => {
+const Index = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState<
+    { question: string; answer: string }[]
+  >([]);
+  const [positiveCounts, setPositiveCounts] = useState({
+    E: 0,
+    S: 0,
+    F: 0,
+    P: 0,
+  });
+  const [showResults, setShowResults] = useState(false);
+
+  const handleAnswer = (answer: string) => {
+    setSelectedAnswers((prevAnswers) => [
+      ...prevAnswers,
+      { question: questions[currentQuestionIndex], answer },
+    ]);
+
+    if (answer === answers[currentQuestionIndex].positive) {
+      if (currentQuestionIndex < 5) {
+        setPositiveCounts((prevCounts) => ({
+          ...prevCounts,
+          E: prevCounts.E + 1,
+        }));
+      } else if (currentQuestionIndex < 10) {
+        setPositiveCounts((prevCounts) => ({
+          ...prevCounts,
+          S: prevCounts.S + 1,
+        }));
+      } else if (currentQuestionIndex < 15) {
+        setPositiveCounts((prevCounts) => ({
+          ...prevCounts,
+          F: prevCounts.F + 1,
+        }));
+      } else if (currentQuestionIndex < 20) {
+        setPositiveCounts((prevCounts) => ({
+          ...prevCounts,
+          P: prevCounts.P + 1,
+        }));
+      }
+    }
+
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  const getResult = () => {
+    const result = [];
+    result.push(positiveCounts.E >= 3 ? "E" : "I");
+    result.push(positiveCounts.S >= 3 ? "S" : "N");
+    result.push(positiveCounts.F >= 3 ? "F" : "T");
+    result.push(positiveCounts.P >= 3 ? "P" : "J");
+    return result.join("");
+  };
+
+  useEffect(() => {
+    if (selectedAnswers.length > 0) {
+      console.log(selectedAnswers);
+    }
+  }, [selectedAnswers]);
+
   return (
     <div className="">
       <Header />
-      <div className="mb-32 mt-32 flex h-screen w-full flex-col items-center gap-32 text-center">
-        <div className="z-10 h-[320px] w-[1000px] content-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor p-8 text-3xl font-bold shadow-xl">
-          1. 이러하고 저러하고 이러하고 저러하면 이렇게
-          하실껀가요?ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-        </div>
-        <div className="flex w-full justify-between p-20">
-          <div className="z-10 h-[80px] w-[180px] content-center items-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor text-2xl font-medium shadow-xl">
-            네
+      <div className="mt-32 flex w-full flex-col items-center text-center">
+        {!showResults ? (
+          <>
+            <div className="z-10 h-[320px] w-[1000px] content-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor p-8 text-3xl font-bold shadow-xl">
+              {questions[currentQuestionIndex]}
+            </div>
+            <div className="flex w-full justify-between gap-28 p-20">
+              <button
+                className="z-10 min-h-[80px] w-full min-w-[250px] content-center items-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor p-2 text-xl font-medium shadow-xl"
+                onClick={() =>
+                  handleAnswer(answers[currentQuestionIndex].positive)
+                }
+              >
+                {answers[currentQuestionIndex].positive}
+              </button>
+              <button
+                className="z-10 min-h-[80px] w-full min-w-[250px] content-center items-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor p-2 text-xl font-medium shadow-xl"
+                onClick={() =>
+                  handleAnswer(answers[currentQuestionIndex].negative)
+                }
+              >
+                {answers[currentQuestionIndex].negative}
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="m-32 w-[700px] content-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor p-8 text-2xl shadow-xl">
+            <h2 className="text-3xl font-bold">답변 결과</h2>
+            <div className="text-4xl">{getResult()}</div>
           </div>
-          <div className="z-10 h-[80px] w-[180px] content-center items-center rounded-lg border-2 border-solid border-smBorderColor bg-smColor text-2xl font-medium shadow-xl">
-            아니요
-          </div>
-        </div>
+        )}
       </div>
       <Footer />
     </div>
   );
 };
 
-export default index;
+export default Index;
--- scent/pages/sign-in/index.tsx
+++ scent/pages/sign-in/index.tsx
@@ -1,7 +1,111 @@
-import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { USER_INPUT_VALIDATION } from "@/constants/input";
+import LoginInput from "@/components/LoginInput";
+import Button from "@/components/Button";
 
-const index = () => {
-  return <div>로그인페이지입니다.</div>;
+interface FormValues {
+  email: string;
+  nickname?: string;
+  password: string;
+  passwordConfirm?: string;
+}
+
+const { email, password } = USER_INPUT_VALIDATION;
+
+const rules = {
+  emailRules: {
+    required: email.errorMessage.empty,
+    pattern: {
+      value: email.regex,
+      message: email.errorMessage.invalid,
+    },
+  },
+  passwordRules: {
+    required: password.errorMessage.empty,
+    pattern: {
+      value: password.regex,
+      message: password.errorMessage.invalid,
+    },
+    minLength: {
+      value: 8,
+      message: password.errorMessage.minLength,
+    },
+    maxLength: {
+      value: 16,
+      message: password.errorMessage.maxLength,
+    },
+  },
 };
 
-export default index;
+const SignUp = () => {
+  const router = useRouter();
+  const { formState, register, handleSubmit } = useForm<FormValues>({
+    mode: "onBlur",
+  });
+
+  const { isValid, errors } = formState;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("회원가입 데이터:", data);
+    router.push("/");
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="flex w-[40rem] flex-col items-center pb-14 pt-14">
+          <div>
+            <Link href="/">
+              <img
+                src="/icon/scentMatching.png"
+                alt="글로벌노마드 로고 이미지"
+                className="h-[180px] w-[250px]"
+              />
+            </Link>
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-10 flex w-full flex-col items-center gap-7 mobile:min-w-80 mobile:px-3"
+          >
+            <LoginInput
+              label="이메일"
+              type="email"
+              placeholder="이메일을 입력해 주세요"
+              isError={!!errors.email}
+              errorMessage={errors.email?.message}
+              {...register("email", rules.emailRules)}
+            />
+            <LoginInput
+              label="비밀번호"
+              type="password"
+              placeholder="비밀번호를 입력해 주세요"
+              isError={!!errors.password}
+              errorMessage={errors.password?.message}
+              {...register("password", rules.passwordRules)}
+            />
+            <Button
+              type="submit"
+              disabled={!isValid}
+              className={`h-12 rounded-md text-base font-bold text-white ${isValid ? "bg-smButtonColor" : "bg-gray-400"}`}
+            >
+              로그인 하기
+            </Button>
+          </form>
+          <div className="text-gnGray800 mt-8 flex gap-2 text-base font-normal">
+            <p>회원이 아니신가요?</p>
+            <Link
+              href="/sign-in"
+              className="text-gnDarkGreen text-base font-normal text-blue-600 underline"
+            >
+              회원가입하기
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
